072f49685e5d8710135ce4e9187bb0e6d3999b23
shiyaowu <shiyao.wu@duke.edu>
2015-02-04 10:39:17 -0500

    A2_update

--- A2_missingdata_duplicated.py
+++ A2_missingdata_duplicated.py
@@ -15,13 +15,19 @@ missing = ['.','','NA','NULL','-']
 df = pd.read_csv(os.path.join(main_dir,csv_file), na_values = missing)
 
 # 2. DROP FULL ROWS THAT ARE DUPLICATED ----------------------------------------
-df.drop_duplicates()
-df.drop_duplicates(take_last = True)
-t_b = df.duplicated()
-df[~t_b]
+df1 = df.drop_duplicates()
 
 # 3. EXTRACT FULL ROWS OF DATA WHERE 'CONSUMP' HAS MISSING VALUES---------------
-rows = df['consump'].isnull()
-df[rows]
+rows = df1['consump'].isnull()
+df1[rows]
 
-# 4. 
+# 4. Check for any duplicated values on the SUBSET of panid and date. Drop the 
+#    rows where consump is missing for any duplicated values
+t_b = df1.duplicated(['panid','date'])
+b_t = df1.duplicated(['panid','date'],take_last = True)
+na = df1['consump'].isnull()
+dup_na = ~((t_b|b_t)&na)
+df2 = df1[dup_na]
+
+# 5 Average of cleaned data
+ave = df2.consump.mean()
